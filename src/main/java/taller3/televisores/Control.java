a79b47c8e98006344a56c3b61ebd51924f92f5fb
HinaraSM <hisanchezm@unal.edu.co>
2022-08-27 21:36:35 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,7 +1,9 @@
 package taller3.televisores;
 
+
 public class Control {
     TV tv;
+    int canal1;
 
     public TV getTv() {
         return tv;
@@ -11,4 +13,28 @@ public class Control {
         this.tv = tv;
     }
 
+    public void enlazar(TV tv){
+        this.tv = tv;
+        tv.setControl(this);
+    }
+
+    public void canalUp(){
+        tv.canalUp();
+    }
+
+    public void canalDown(){
+        tv.canalDown();
+    }
+
+    public void volumenUp(){
+        tv.volumenUp();
+    }
+
+    public void volumenDown(){
+        tv.volumenDown();
+    }
+
+    public void setCanal(int canal1){
+        tv.setCanal(canal1);
+    }
 }
