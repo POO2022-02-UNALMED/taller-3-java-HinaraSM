49cc48945b31463fb9c119023c81aa0668ab2722
HinaraSM <69545933+HinaraSM@users.noreply.github.com>
2022-08-27 22:16:48 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -2,7 +2,7 @@ package taller3.televisores;
 
 public class TV {
     Marca marca;
-    public int canal=1;
+    int canal=1;
     int precio=500;
     boolean estado;
     int volumen=1;
@@ -97,65 +97,4 @@ public class TV {
             this.canal-=1;
         }
     }    
-}
-
-    public void setPrecio(int precio) {
-        this.precio = precio;
-    }
-
-    public int getVolumen() {
-        return volumen;
-    }
-
-    public void setVolumen(int volumen) {
-        this.volumen = volumen;
-    }
-
-    public Control getControl() {
-        return control;
-    }
-
-    public void setControl(Control control) {
-        this.control = control;
-    }
-
-    public static int getNumTV() {
-        return numTV;
-    }
-
-    public void turnOn() {
-        this.estado=true;
-    }
-    
-    public void turnOff() {
-        this.estado=false;
-    }
-
-    public boolean getEstado() {
-        return estado;
-    }
-
-    public void canalUp(){
-        if (canal<120){
-            this.canal+=1;
-        }
-    }
-
-    public void canalDown(){
-        if (canal>1){
-            this.canal-=1;
-        }
-    }
-
-    public void volumenUp(){
-        if (volumen<7){
-            this.canal+=1;
-        }
-    }
-
-    public void volumenDown(){
-        if (volumen>0){
-            this.canal-=1;
-        }
-    }
 }
