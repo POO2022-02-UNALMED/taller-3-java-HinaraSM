dd15f7f30636f613c6c7d553d32fc3541f372e86
HinaraSM <hisanchezm@unal.edu.co>
2022-08-27 21:11:15 -0500

    Clases

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,14 @@
+package taller3.televisores;
+
+public class Control {
+    TV tv;
+
+    public TV getTv() {
+        return tv;
+    }
+
+    public void setTv(TV tv) {
+        this.tv = tv;
+    }
+
+}
